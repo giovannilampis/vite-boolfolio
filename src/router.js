a09ff3971f762ab5595555ba8a48e43ef734cf43
giovannilampis <giolampis@hotmail.com>
2023-07-01 10:58:28 +0200

    add SingleProject.vue to pages

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
+import SingleProject from './pages/SingleProject.vue';
 
 const router = createRouter({
     history: createWebHistory(),
